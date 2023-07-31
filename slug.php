1d3d842a3ff593581243e33125150fd98c705516
Matheus <matheusstos123456@gmail.com>
2023-07-31 20:34:34 -0300

    Composer

---
+++ slug.php
@@ -0,0 +1,9 @@
+<?php 
+require_once 'vendor/autoload.php';
+
+use Cocur\Slugify\Slugify;
+
+$slugify = new Slugify();
+echo $slugify->slugify('Teste com acentuação para geração de slug', '/');
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7aa23b73b394ab4678d26a594a63fea3405529d4',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7aa23b73b394ab4678d26a594a63fea3405529d4',
+            'dev_requirement' => false,
+        ),
+        'cocur/slugify' => array(
+            'pretty_version' => 'v3.2',
+            'version' => '3.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cocur/slugify',
+            'aliases' => array(),
+            'reference' => 'd41701efe58ba2df9cae029c3d21e1518cc6780e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
